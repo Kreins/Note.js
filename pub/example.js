7794e96d3404ef6af04564140d777e3f3497051e
Jun Da Wang <junda.wang@mail.utoronto.ca>
2020-11-22 16:04:34 -0500

    landing page display toast globally

--- pub/example.js
+++ pub/example.js
@@ -1,7 +1,14 @@
 "use strict";
 
-const note = new Note();
-const toast = note.makeToast("hi");
+const noteGen = new Note();
+const toast = noteGen.makeToast("hello");
 const target = $("#target");
 
-const first = note.createNote();
+$(".topCenter").click(() => toast.display("top-center"));
+$(".topRight").click(() => toast.display("top-right"));
+$(".midRight").click(() => toast.display("mid-right"));
+$(".botRight").click(() => toast.display("bot-right"));
+$(".botCenter").click(() => toast.display("bot-center"));
+$(".botLeft").click(() => toast.display("bot-left"));
+$(".midLeft").click(() => toast.display("mid-left"));
+$(".topLeft").click(() => toast.display("top-left"));
