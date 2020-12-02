aa4509d36c47face5b04957dfbe37a92736df5d9
Jun Da Wang <junda.wang@mail.utoronto.ca>
2020-12-02 15:29:05 -0500

    refactor code

--- pub/Note.js
+++ pub/Note.js
@@ -6,7 +6,7 @@ let defaultOptions = {
   duration: 3000,
 };
 
-let generateID = function () {
+let _generateID = function () {
   // Math.random should be unique because of its seeding algorithm.
   // Convert it to base 36 (numbers + letters), and grab the first 9 characters
   // after the decimal.
@@ -14,7 +14,7 @@ let generateID = function () {
 };
 
 function Note(content) {
-  this._id = generateID();
+  this._id = _generateID();
   this.content = content;
   this.position = "";
   this.texts = [];
@@ -34,8 +34,7 @@ Note.prototype.makeToast = function (text) {
   return newNote;
 };
 
-/* -----------------------------------*/
-
+/* -------- initialize empty note ---------*/
 Note.prototype.createNote = function (content) {
   const newNote = new Note(
     $("<div class='default-note-wrapper'></div>").append(content)
@@ -44,6 +43,7 @@ Note.prototype.createNote = function (content) {
   return newNote;
 };
 
+/* ============= text features ============= */
 Note.prototype.addText = function (text) {
   const textNode = $("<div class='text-wrapper'></div>").append(
     document.createTextNode(text)
@@ -73,6 +73,7 @@ Note.prototype.removeText = function (index) {
   return this;
 };
 
+/* ============ button features ============== */
 const _makeButton = function (buttonText) {
   const newButton = $("<button class='default-button'></button>")
     .wrap("<div class='button-wrapper'></div>")
@@ -102,6 +103,91 @@ Note.prototype.removeButtons = function (buttonText) {
   return this;
 };
 
+/* =========== display note features ============ */
+const _setGlobalPosition = function (note, position) {
+  switch (position) {
+    case "top-left":
+      note.content.addClass("top-left");
+      note.position = "top-left";
+      break;
+    case "top-right":
+      note.content.addClass("top-right");
+      note.position = "top-right";
+      break;
+    case "top-center":
+      note.content.addClass("top-center");
+      note.position = "top-center";
+      break;
+    case "bot-left":
+      note.content.addClass("bot-left");
+      note.position = "bot-left";
+      break;
+    case "bot-right":
+      note.content.addClass("bot-right");
+      note.position = "bot-right";
+      break;
+    case "bot-center":
+      note.content.addClass("bot-center");
+      note.position = "bot-center";
+      break;
+    case "mid-left":
+      note.content.addClass("mid-left");
+      note.position = "mid-left";
+      break;
+    case "mid-right":
+      note.content.addClass("mid-right");
+      note.position = "mid-right";
+      break;
+  }
+};
+
+const _setAttachedPosition = function (note, target, position) {
+  note.content.addClass("attached");
+
+  const targetWidth = target.outerWidth();
+  const targetHeight = target.outerHeight();
+  const contentWidth = note.content.outerWidth();
+  const contentHeight = note.content.outerHeight();
+
+  switch (position) {
+    case "top-left":
+      // TODO
+      break;
+    case "top-right":
+      // TODO
+      break;
+    case "top-center":
+      note.content
+        .css("left", (targetWidth - contentWidth) / 2)
+        .css("top", -contentHeight - 10); // 10px between target and content
+      note.position = "elem-top-center";
+      break;
+    case "bot-left":
+      // TODO
+      break;
+    case "bot-right":
+      // TODO
+      break;
+    case "bot-center":
+      note.content
+        .css("top", targetHeight + 10)
+        .css("left", (targetWidth - contentWidth) / 2); // 10px between target and content
+      note.position = "elem-bot-center";
+      break;
+    case "mid-left":
+      note.content
+        .css("top", (targetHeight - contentHeight) / 2)
+        .css("left", -contentWidth - 10); // 10px between target and content
+      note.position = "elem-mid-left";
+      break;
+    case "mid-right":
+      note.content
+        .css("top", (targetHeight - contentHeight) / 2)
+        .css("left", targetWidth + 10); // 10px between target and content
+      note.position = "elem-mid-right";
+      break;
+  }
+};
 Note.prototype.display = function (
   position = "mid-left",
   target = "body",
@@ -121,50 +207,13 @@ Note.prototype.display = function (
 
   content.hide();
 
+  /* set positions and add to DOM */
   if (target === "body") {
-    switch (position) {
-      case "top-left":
-        content.addClass("top-left");
-        this.position = "top-left";
-        break;
-      case "top-right":
-        content.addClass("top-right");
-        this.position = "top-right";
-        break;
-      case "top-center":
-        content.addClass("top-center");
-        this.position = "top-center";
-        break;
-      case "bot-left":
-        content.addClass("bot-left");
-        this.position = "bot-left";
-        break;
-      case "bot-right":
-        content.addClass("bot-right");
-        this.position = "bot-right";
-        break;
-      case "bot-center":
-        content.addClass("bot-center");
-        this.position = "bot-center";
-        break;
-      case "mid-left":
-        content.addClass("mid-left");
-        this.position = "mid-left";
-        break;
-      case "mid-right":
-        content.addClass("mid-right");
-        this.position = "mid-right";
-        break;
-    }
+    // attach to body
+    _setGlobalPosition(note, position);
     $("body").append(content);
   } else {
-    /* attach to target element */
-
-    // first wrap target into a div and append
-    // the note as its child
-    const targetWidth = target.outerWidth();
-    const targetHeight = target.outerHeight();
-
+    // attach to target element
     /* TODO: if target element has a border, then its padding should be taken into account when placing the note*/
     // const padding_top = parseInt(target.css("padding-top"));
     // const padding_bottom = parseInt(target.css("padding-bottom"));
@@ -172,65 +221,14 @@ Note.prototype.display = function (
     // const border_bottom = parseInt(target.css("border-bottom-width"));
     if (!target.prev().hasClass("target-wrapper")) {
       const targetWrapper = $("<div class='target-wrapper'></div>")
-        .css("width", targetWidth + 1)
-        .css("height", targetHeight + 1);
+        .css("width", target.outerWidth() + 1)
+        .css("height", target.outerHeight() + 1);
       target.before(targetWrapper);
     }
-
-    // if (!target.parent().hasClass("target-wrapper")) {
-    //   target
-    //     .wrap("<div class='target-wrapper'></div>")
-    //     .parent()
-    //     .css("width", targetWidth + 1)
-    //     .css("height", targetHeight + 1);
-    // } //TODO: target move slightly after wrapped
-
     target.prev().append(content);
-
-    content.addClass("attached");
-
-    const contentWidth = content.outerWidth();
-    const contentHeight = content.outerHeight();
-
-    switch (position) {
-      case "top-left":
-        // TODO
-        break;
-      case "top-right":
-        // TODO
-        break;
-      case "top-center":
-        content
-          .css("left", (targetWidth - contentWidth) / 2)
-          .css("top", -contentHeight - 10); // 10px between target and content
-        this.position = "elem-top-center";
-        break;
-      case "bot-left":
-        // TODO
-        break;
-      case "bot-right":
-        // TODO
-        break;
-      case "bot-center":
-        content
-          .css("top", targetHeight + 10)
-          .css("left", (targetWidth - contentWidth) / 2); // 10px between target and content
-        this.position = "elem-bot-center";
-        break;
-      case "mid-left":
-        content
-          .css("top", (targetHeight - contentHeight) / 2)
-          .css("left", -contentWidth - 10); // 10px between target and content
-        this.position = "elem-mid-left";
-        break;
-      case "mid-right":
-        content
-          .css("top", (targetHeight - contentHeight) / 2)
-          .css("left", targetWidth + 10); // 10px between target and content
-        this.position = "elem-mid-right";
-        break;
-    }
+    _setAttachedPosition(note, target, position);
   }
+
   // fade in and fade out
   content.fadeIn(function () {
     // zero duration => doesn't fade out
